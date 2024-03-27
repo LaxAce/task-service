485ee02608cc4b0e6610a5bee2d561971c7b230b
Abdulhafeez <laxhameedhafeez@gmail.com>
2024-03-27 13:56:25 +0100

    let get started!!!

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 2024;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(`Hello World! this is ${port}`);
+  res.send(`Hello World!`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
